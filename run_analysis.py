25e26e1f95b1a472e3c78370f9255adf46853726
rjanish <ryan.janish@gmail.com>
2023-09-13 11:28:07 -0500

    adding coordinates

--- run_analysis.py
+++ run_analysis.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.io.fits as fits 
+import astropy.table as table
 
 import MWDMhalo as mw
 
@@ -14,14 +15,26 @@ project_dir = "/home/rjanish/physics/optical-ir-axion-decay"
 JWSTdata_dir = os.path.join(project_dir, "/data/mastDownload/JWST")
 
 def get_target_list(datadir):
+    """ 
+    compile table of targets and important targe data 
+    """
+    # find all data files 
     data_filenames = []
     for current_path, current_dirnames, current_filenames in os.walk(datadir):
         data_filenames += current_filenames
-
+    # start table with metadata from data files  
+    targets = table.Table(
+        names=("name", "ra", "dec", "instrument", 
+               "detector", "filter", "grating", "int_time"), 
+        dtype=(str, float, float, str, 
+               str, str, str, float))
     for filename in data_filenames:
-        hdul=fits.open(datafile_path)
-        ra = hdul[0].header["RA"]
-        dec = hdul[0].header["DEC"]
+        with fits.open(datafile_path) as hdul:
+            targets.add_row(
+                (hdul[0].header["TARGNAME"], hdul[0].header["TARG_RA"], 
+                 hdul[0].header["TARG_DEC"], hdul[0].header["INSTRUME"], 
+                 hdul[0].header["DETECTOR"], hdul[0].header["FILTER"], 
+                 hdul[0].header["GRATING"], hdul[0].header["EFFINTTM"]))
 
 
 if __name__ == "__main__":
