3e2d967574ca3d5db8209039d94b6d8599253a3f
rjanish <ryan.janish@gmail.com>
2023-10-21 00:51:02 -0500

    fix continuum refit bug

--- check-detections.py
+++ check-detections.py
@@ -19,22 +19,24 @@ print("num detections: {}".format(Ndetect))
 for detection in bestfits[detections, :]:
 	lam = detection[0]
 	chisq = detection[4]
-	print("\nlam = {:0.2f}, d(chisq) = {:0.2f}"
+	best_g = detection[3]
+	print("\nlam = {:0.4f}, d(chisq) = {:0.1f}"
 		  "".format(lam, chisq))
 	# global significance
 	p_local = p(chisq)
 	p_global = p_local*num_trials
 	Z_global = np.sqrt(2)*erfcinv(2*p_global)
-	print("    sigma_local  = {:0.2f}\n" 
-		  "    sigma_global = {:0.2f}"
-		  "".format(np.sqrt(chisq), Z_global))
+	print("    sigma_local  = {:0.1f}\n" 
+		  "    sigma_global = {:0.1f}\n"
+		  "    best_rate    = {:0.2e}"
+		  "".format(np.sqrt(chisq), Z_global, best_g))
 
 
 
-# limits diagonostics 
-limits_path = "gnz11_final/continuum/JWST-NIRSPEC-limits.dat"
-limits = np.loadtxt(limits_path)
-strongest_m = bestfits[np.argsort(limits[:, 2]), 0]
-strongest_g = limits[np.argsort(limits[:, 2]), 2]
-for l, g in zip(strongest_m[:10], strongest_g[:10]):
-	print(l, g)
+# # limits diagonostics 
+# limits_path = "gnz11_final/continuum/JWST-NIRSPEC-limits.dat"
+# limits = np.loadtxt(limits_path)
+# strongest_m = bestfits[np.argsort(limits[:, 2]), 0]
+# strongest_g = limits[np.argsort(limits[:, 2]), 2]
+# for l, g in zip(strongest_m[:10], strongest_g[:10]):
+# 	print(l, g)
--- sub-line-fit-full-multi.py
+++ sub-line-fit-full-multi.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import scipy.optimize as opt
 import scipy.interpolate as interp
+import scipy.integrate as integ 
 
 import DMdecayJWST as assume
 import JWSTparsedatafiles as JWSTparse 
@@ -92,20 +93,23 @@ def find_raw_limit(setup_params, data, lam0):
     fixed_list = []
     spec_list = []
     res_list = []
+    lstart = [spec["lam"][0] for spec in data]
+    lend = [spec["lam"][-1] for spec in data]
     lmin = lam0*(1.0 - 0.5*window)
     lmax = lam0*(1.0 + 0.5*window)
-    print(lmin, lam0, lmax)
     for i, spec in enumerate(data):
-        print(lstart[i], lend[i])
         if (lam0 < lstart[i]) or (lam0 > lend[i]):
             continue
         if lmin < lstart[i]:
-            lmin = lstart[i]
-            lmax = lmin*(1 + window)
+            l_left = lstart[i]
+            l_right = l_left*(1 + window)
         elif lend[i] < lmax:
-            lmax = lend[i]
-            lmin = lmax*(1 - window)
-        select = (lmin < spec["lam"]) & (spec["lam"] < lmax)
+            l_right = lend[i]
+            l_left = l_right*(1 - window)
+        else:
+            l_left = lmin
+            l_right = lmax
+        select = (l_left < spec["lam"]) & (spec["lam"] < l_right)
         spec_list.append(i)
         sky_list.append(spec["sky"][select])
         lam_list.append(spec["lam"][select])
@@ -114,9 +118,6 @@ def find_raw_limit(setup_params, data, lam0):
                            sigma_from_fwhm(spec["max_res"], lam0)])    
         res_list.append(spec["max_res"])
     num_specs = len(sky_list)
-    print(num_specs)
-    print(lmin, lam0, lmax)
-    print()
     if num_specs == 0:
         return [[lmin, lmax], spec_list, [None], [None], 
                 [np.nan, None], [np.nan, None], np.nan]
@@ -189,9 +190,15 @@ def find_raw_limit(setup_params, data, lam0):
         lam_list_msk.append(lam_list[i][~to_mask])
         error_list_msk.append(error_list[i][~to_mask])
 
+    # re-fit continuum
+    spline_fit = opt.least_squares(
+        spline_residual, best_knot_values,
+        args=(knots, lam_list_msk, sky_list_msk, error_list_msk))
+    refit_best_knot_values = spline_fit["x"]
+
         
     # fit continuum(s) + line
-    guess = np.concatenate((best_knot_values, [limit_guess[0]]))
+    guess = np.concatenate((refit_best_knot_values, [limit_guess[0]]))
     upper_bound = np.inf*np.ones(guess.size)    
     lower_bound = -np.inf*np.ones(guess.size)
     lower_bound[-num_specs:] = 0.0
@@ -207,16 +214,13 @@ def find_raw_limit(setup_params, data, lam0):
     best_fit_chisq = line_chisq(best_rate, best_knots, knots, 
                                 fixed_list, lam_list_msk, 
                                 sky_list_msk, error_list_msk)
-    no_fit_chisq = line_chisq(0.0, best_knot_values, knots, 
+    no_fit_chisq = line_chisq(0.0, refit_best_knot_values, knots, 
                               fixed_list, lam_list_msk, 
                               sky_list_msk, error_list_msk)
     delta_chisq = no_fit_chisq - best_fit_chisq
 
     # marganilize over continuum params 
-    best_chisq = line_chisq(best_rate, best_knots, knots, 
-                            fixed_list, lam_list_msk, sky_list_msk, 
-                            error_list_msk)
-    threshold = chisq_step + best_chisq
+    threshold = chisq_step + best_fit_chisq
     knot_bounds = np.zeros((knots.size, 2))
     for i in range(num_specs):
         error = np.median(error_list[i])
@@ -243,7 +247,7 @@ def find_raw_limit(setup_params, data, lam0):
 def find_pc_limit(Ntrials, Nbins, power_threshold, 
                  setup_params, data, fit_region):
     [lam0, lam_list_msk, error_list_msk, 
-     knots, best_knots] = fit_region
+     knots, best_knots, spec_list] = fit_region
     num_knots = setup_params["num_knots"]
     guess = np.concatenate((best_knots, [0.0])) # fit starting
 
@@ -252,10 +256,10 @@ def find_pc_limit(Ntrials, Nbins, power_threshold,
     for trial in range(Ntrials):
         mock_data = []
         trial_sky_list_msk = []
-        for i in range(len(data)):
+        for i, spec_i in enumerate(spec_list):
             mock_data.append({})
-            mock_data[-1]["max_res"] = data[i]["max_res"]
-            mock_data[-1]["D"] = data[i]["D"]
+            mock_data[-1]["max_res"] = data[spec_i]["max_res"]
+            mock_data[-1]["D"] = data[spec_i]["D"]
             mock_data[-1]["lam"] = lam_list_msk[i]
             mock_data[-1]["error"] = error_list_msk[i]
             start = i*num_knots
@@ -269,7 +273,6 @@ def find_pc_limit(Ntrials, Nbins, power_threshold,
         upper_limits[trial] = out[4][0]
 
     # integrate histogram
-    print(upper_limits)
     hist = np.histogram(upper_limits, bins=Nbins)
     midpoints = 0.5*(hist[1][1:] +  hist[1][:-1])
     pdf  = interp.interp1d(midpoints, hist[0], 
@@ -296,30 +299,29 @@ if __name__ == "__main__":
     setup_params["window"] = width_factor*v_dm
     setup_params["padding"] = 1e-8
     setup_params["num_knots"] = 5
-    setup_params["chisq_step"] = 2.71 #4
+    setup_params["chisq_step"] = 4 #2.71 
     setup_params["limit_guess"] = [1e-5, 1e-3]
     setup_params["max_clip_iters"] = 100
-    setup_params["clipping_factor"] = 2
+    setup_params["clipping_factor"] = 3
     # pc limit params 
-    pc_step_factor = 6 # subsampling factor for computing pc limit
+    pc_step_factor = 5 # subsampling factor for computing pc limit
     Ntrials = 10**2
     Nbins = 15
     power_threshold = 0.1587
     # run params
-    Nthreads = 1
-    inflate = 50   # set > 1 to undersample mass range for testing
+    Nthreads = 4
+    inflate = 1   # set > 1 to undersample mass range for testing
 
     # generate mass sampling
-    lstart = [spec["lam"][0] for spec in data]
-    lend = [spec["lam"][-1] for spec in data]
-    l_initial = np.min(lstart)
-    l_final = np.max(lend)
+    l_initial = np.min([spec["lam"][0] for spec in data])
+    l_final = np.max([spec["lam"][-1] for spec in data])
     test_lams = [l_initial]
     while test_lams[-1] < l_final:
         dlam_i = 2*np.min([sigma_from_fwhm(spec["max_res"], test_lams[-1]) 
                            for spec in data])*inflate 
         test_lams.append(test_lams[-1] + dlam_i)
     test_lams = np.asarray(test_lams[1:-1]) # strip to stay inside data bounds
+    # test_lams = [1.1401]
 
     # get raw, non-pc limited bounds
     raw_limits_func = functools.partial(find_raw_limit, setup_params, data)
@@ -330,11 +332,12 @@ if __name__ == "__main__":
     limits = np.asarray([out[4][0] for out in raw_output])
     bestfit = np.asarray([out[5][0] for out in raw_output])
     delta_chisqs = np.asarray([out[6] for out in raw_output])
-    dt = time.time() - t0
-    print("{:0.2f} sec".format(dt))
+    error_scale_factors = np.asarray([out[3] for out in raw_output])
+    dt_raw = time.time() - t0
+    print("{:0.2f} sec".format(dt_raw))
 
     # compute pc limits 
-    pc_inputs = [[out[9], out[7], out[8], out[2], out[5][1]] 
+    pc_inputs = [[out[9], out[7], out[8], out[2], out[5][1], out[1]] 
                  for out in raw_output[::pc_step_factor]]
     pc_limits_func = functools.partial(find_pc_limit, Ntrials, Nbins, 
                                        power_threshold, setup_params, data)
@@ -342,23 +345,32 @@ if __name__ == "__main__":
     t0 = time.time()
     with mltproc.Pool(Nthreads) as pool:
         pc_output = pool.map(pc_limits_func, pc_inputs)    
-    dt = time.time() - t0
+    dt_pc = time.time() - t0
     pc_limits = np.asarray(pc_output)
-    print("{:0.2f} sec".format(dt))
-    for l, g in pc_limits:
-        print("{:0.2f}    {:0.2e}".foramt(l , g))
+    print("{:0.2f} sec".format(dt_pc))
+
+    # interpolate pc limits 
+    pc_limit_func = interp.interp1d(pc_limits[:, 0], pc_limits[:, 1],
+                                    bounds_error=False, 
+                                    fill_value=(pc_limits[0, 1], 
+                                                pc_limits[1, 1]))
+    full_pc_limits = pc_limit_func(test_lams)
+    final_limits = np.max([limits, full_pc_limits], axis=0)
+    pc_hit = np.argmax([limits, full_pc_limits], axis=0)
+    print("total: {:0.2f} mins".format((dt_pc + dt_raw)/60.0))
+    print("power constrained fraction: ", np.sum(pc_hit)/pc_hit.size)
 
     # physical conversion 
     m = convert.wavelength_to_mass(test_lams)
     limit_decayrate = convert.fluxscale_to_invsec(
-        limits, assume.rho_s, assume.r_s)    
+        final_limits, assume.rho_s, assume.r_s)    
     limit_g = convert.decayrate_to_axion_g(limit_decayrate, m) 
     bestfit_decayrate = convert.fluxscale_to_invsec(
         bestfit, assume.rho_s, assume.r_s)    
     bestfit_g = convert.decayrate_to_axion_g(bestfit_decayrate, m) 
 
     # write output 
-    run_name = "gnz11_ngc6552_final"
+    run_name = "gnz11_final"
     line_results_dir = "{}/continuum".format(run_name)
 
     limits_path = ("{}/JWST-NIRSPEC-limits.dat"
@@ -385,4 +397,18 @@ if __name__ == "__main__":
                                 bestfit_decayrate, bestfit_g,
                                 delta_chisqs)),
                header=bestfits_header)
+
+    pc_path = ("{}/JWST-NIRSPEC-pc.dat"
+                     "".format(line_results_dir))
+    pc_header = ("DM decay power constraint results vs mass \n"
+                       "JWST NIRSPEC run {}\n"
+                       "lambda0 [micron]    mass [ev]    "    
+                       "raw limit [comp units]    "
+                       "pc limit [comp units]    "
+                       "pc used"
+                       "".format(run_name))
+    np.savetxt(pc_path, 
+               np.column_stack((test_lams, m, limits, 
+                                full_pc_limits, pc_hit)),
+               header=pc_header)
 
