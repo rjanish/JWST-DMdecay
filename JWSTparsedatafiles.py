fix D-scaling confusion

JWSTparsedatafiles.py
@@ -82,8 +82,8 @@ def process_target_list(datafile_paths):
     for index in range(Ntargets):
         Ds[index] = mw.compute_halo_Dfactor(
             targets[index]["b"], targets[index]["l"],
-            mw.NFWprofile, assume.r_sun/assume.r_s)
-        data[index]["D"] = Ds[index]*assume.rho_s*assume.r_s
+            mw.NFWprofile, assume.r_sun/assume.r_s)*assume.rho_s*assume.r_s
+        data[index]["D"] = Ds[index]
     targets["D"] = Ds
     # compute min distance from galactic center
     b_rad = targets["b"]*(np.pi/180.0)

conversions.py
@@ -7,35 +7,49 @@ def wavelength_to_mass(lam):
     convert two-photon decay wavelength in microns to 
     progeniter mass in ev, via lam = 4pi/m
     """
-    return 2.46/lam
+    return 2.46/lam # eV
 
-def fluxscale_to_invsec(limit, rho_s, r_s):
+def fluxscale_to_invsec(limit):
     """ 
-    Convert flux scale limit in MJy/(sr micron kpc GeV/cm^3)
-    do a decay rate in 1/sec. This assumes the D-factors 
-    were normalized by the given rho_s and r_s values.
-    The "flux scale" G is 
-      dphi/dnu dOmega = 
-          (G/4pi) (df/dnu * W) D
-    where D here is dimensionless, its overall scale being factored
-    out as rho_s r_s, df/dnu is the decay spectrum and W the 
-    instrumental response function.  G is thus related to the 
-    decay rate Gamma as G = Gamma rho_s r_s.  
-
-    Double check all these units!!
+    The code computes the decay rate G via 
+        Phi = dphi/(dnu dOmega)
+            = (G/4pi) (df/dnu * W) D
+    The quantites are input in the following units:
+        D = Din GeV/cm^3 kpc
+        (df/dnu * W) = S_in micron
+    Note that though df/dnu is 'per frequency', we express it and 
+    then evaluate it in the code as a function of wavelength, so
+    its units are in microns, the wavelenth unit used in the code. 
+        Phi = Phi_in MJy 
+    This is "really" MJy/sr, but the sr is in a sense conventional
+    so we drop it in this comparison.  Another way to see it: suppose
+    the instrument gave us a measure of Phi in MJy/sr over a solid 
+    angle Omega.  Then we would compute  
+        Phi Omega = (G/4pi) (df/dnu * W) D Omega
+    and both sides are now in honest-to-God MJy, no sr involved. 
+    But this is exactly equilavent to the above.  So G is computed 
+    by the code to be 
+        G_out = (4 pi Phi_in) / (S_in D_in)
+    and the units of G_out are 
+        G = G_out (MJy cm^3 / micron GeV kpc) 
+    We want to convert this into 1/sec, 
+        G = Ginvsec (1/sec)
+        Ginvsec = G_out (MJy cm^3 sec/ micron GeV kpc) 
+    The quantity in parentesis is unitless, and is the scale 
+    factor used here. See unit-converstion.nb for evaluation. 
     """
-    return limit*(5.93e-22)/(rho_s*r_s)
+    return limit*(5.93e-22) # 1/sec
 
 def decayrate_to_axion_g(rate, m):
     """ 
     convert decay rate in sec^-1 and axion mass in eV to the
     axion-two photon coupling in GeV^-1 
     """ 
-    return 366.1*np.sqrt(rate)/(m**1.5)
+    return 366.1*np.sqrt(rate)/(m**1.5) # 1/GeV
 
 def axion_g_to_decayrate(g, m):
     """ 
     convert axion-two photon coupling in GeV^-1 and axion mass 
     in eV to the decay rate in sec^-1    
     """ 
-    return (m**3)*(g/366.1)**2
+    return (m**3)*(g/366.1)**2 # 1/sec

make_constraint_plot.py
@@ -39,7 +39,7 @@ labels = {"Globular Clusters":["Stellar Evolution", 4.6e-1, 5.5e-11, "0.4"],
           # "HST COB":["HST", 8e0, 3e-11, "0.5"],
           "MUSE":["MUSE", 3.1e0, 2e-11, "0.8"],
           "VIMOS":["VIMOS", 4.7e0, 2e-11, "0.4"],
-          "CAST":["CAST", 5.5e-1, 1e-9, "0.8"]}
+          "CAST":["CAST", 5.5e-1, 4e-10, "0.8"]}
 
 if __name__ == "__main__":
     run_name = sys.argv[1]    
@@ -60,7 +60,7 @@ if __name__ == "__main__":
             limit_data[name] = np.loadtxt(path, delimiter=",")
 
     lower_edge = 1e-13
-    upper_edge = 5e-9
+    upper_edge = 1e-9
     left_edge = 4.5e-1
     right_edge = 6e0
     fig, [ax_t, ax_g] = plt.subplots(2, 1)
@@ -102,6 +102,7 @@ if __name__ == "__main__":
     t0 = 2e3 #sec
     efficency = 0.01
     width = 150
+    print("estimate for g:")
     for t, s in zip(time, style):
         scaled_limit = line_limit[:, 2]*(t*efficency/t0)**(-0.25)
         smoothed_limit = np.exp(
@@ -109,33 +110,38 @@ if __name__ == "__main__":
         ax_g.plot(line_limit[:, 0], smoothed_limit,
             color="blue", linewidth=1, linestyle=s,
             marker='', alpha=0.6) 
+        print("    {:d} yr:  {:0.2e} - {:0.2e}"
+              "".format(int(t/3e7), 
+                        np.min(smoothed_limit), 
+                        np.max(smoothed_limit)))
+    print()
 
     ax_g.set_ylim([lower_edge, upper_edge])
     ax_g.set_xlim([left_edge, right_edge])
     ax_g.set_yscale('log')
     ax_g.set_xscale('log')
 
-    ax_g.text(1e0, 6.5e-11, 
+    ax_g.text(1e0, 13e-11, 
             "Total Flux", 
             color="black",
             fontsize=13, rotation=0)
 
-    ax_g.text(5.4e-1, 1.2e-11, 
-        "Continuum", 
+    ax_g.text(5e-1, 1e-11, 
+        "Continuum Model", 
         color="firebrick",
         fontsize=14, rotation=0)
-    ax_g.text(6.35e-1, 5e-12, 
-        "Model", 
-        color="firebrick",
-        fontsize=13, rotation=0)
+    # ax_g.text(6.35e-1, 6e-12, 
+    #     "Model", 
+    #     color="firebrick",
+    #     fontsize=13, rotation=0)
 
 
-    ax_g.text(2.15e0, 2.8e-12, 
+    ax_g.text(2.15e0, 4.7e-12, 
         "Current", 
         color="darkblue",
         fontsize=12, rotation=0)
 
-    ax_g.text(8e-1, 7e-13, 
+    ax_g.text(8e-1, 1.5e-12, 
         "15 year", 
         color="darkblue",
         fontsize=13, rotation=0)
@@ -147,7 +153,7 @@ if __name__ == "__main__":
                   fontsize=16)
 
 
-    lifetime_upper_edge = 1e30
+    lifetime_upper_edge = 4e29
     lifetime_lower_edge = 1e24
 
     for name in decay_only:
@@ -171,6 +177,7 @@ if __name__ == "__main__":
             color="Black", linewidth=2)
 
     # scale estimate 
+    print("estimate for tau:")
     for t, s in zip(time, style):
         scaled_limit = (line_limit[:, 1]**-1)*(t*efficency/t0)**(0.5)
         smoothed_limit = np.exp(
@@ -178,28 +185,32 @@ if __name__ == "__main__":
         ax_t.plot(line_limit[:, 0], smoothed_limit,
             color="blue", linewidth=1, linestyle=s,
             marker='', alpha=0.6) 
+        print("    {:d} yr:  {:0.2e} - {:0.2e}"
+              "".format(int(t/3e7), 
+                        np.min(smoothed_limit), 
+                        np.max(smoothed_limit)))
 
-    ax_t.text(9e-1, 1e25, 
+    ax_t.text(9e-1, 0.25e25, 
             "Total Flux", 
             color="black",
             fontsize=14, rotation=0)
 
-    ax_t.text(5.4e-1, 1e27, 
+    ax_t.text(5.4e-1, 0.25e27, 
         "Continuum", 
         color="firebrick",
         fontsize=14, rotation=0)   
 
-    ax_t.text(6.25e-1, 3.5e26, 
+    ax_t.text(6.25e-1, 1e26, 
         "Model", 
         color="firebrick",
         fontsize=14, rotation=0)
 
-    ax_t.text(6.1e-1, 1.5e28, 
+    ax_t.text(6.1e-1, 0.25e28, 
         "Current", 
         color="darkblue",
         fontsize=14, rotation=0)
 
-    ax_t.text(2e0, 2.2e28, 
+    ax_t.text(2e0, 0.5e28, 
         "15 year", 
         color="darkblue",
         fontsize=14, rotation=0)

run_analysis.py
@@ -30,7 +30,10 @@ def chisq_dm_totalflux(rate, lam0, fixed_list, x_list,
         resid = np.zeros(model.shape)
         dm_large = model > y_list[i]
         resid[dm_large] = ((model[dm_large] - y_list[i][dm_large]) / 
-                           (2.5*sigma_y_list[i][dm_large])) # scaled error
+                           (sigma_y_list[i][dm_large])) 
+            # use unscaled error - this doesn't matter much here, 
+            # the result depends on the error at less-than-O(1)
+            # level so long as error < flux
         total += np.sum(resid**2)
     return total - shift
 
@@ -65,7 +68,7 @@ if __name__ == "__main__":
     #         targets = targets[select]
 
     # data, targets = assume.parse_gnz11()
-    data, targets = assume.parse_sub(assume.all_paths)
+    data, targets = assume.parse_sub(assume.gnz11_paths )
 
     targets.write("{}/targets.html".format(run_name), 
                   format="ascii.html", overwrite=True)
@@ -74,7 +77,7 @@ if __name__ == "__main__":
     Nrows = len(data)
     print("analyzing {} spectra".format(Nrows))
 
-    chisq_threshold = 4
+    chisq_threshold = 2.71 #4
 
     extract_list = lambda list_of_dicts, key: [d[key] for d in list_of_dicts]
     sky_list = extract_list(data, "sky")
@@ -94,7 +97,7 @@ if __name__ == "__main__":
     lam_test = [l_initial]
     while lam_test[-1] < l_final:
         dlam_i = 2*np.min([sigma_from_fwhm(spec["max_res"], lam_test[-1]) 
-                           for spec in data])*50 #rescale for testing
+                           for spec in data]) #rescale for testing
         lam_test.append(lam_test[-1] + dlam_i)
     lam_test = np.asarray(lam_test[1:-1]) # the last one is always outside the range
 
@@ -145,7 +148,7 @@ if __name__ == "__main__":
     print("total time: {:0.1f} min".format(dt_total))
     # convert to physics units
     m = convert.wavelength_to_mass(lam_test)
-    limit_decayrate = convert.fluxscale_to_invsec(limit, assume.rho_s, assume.r_s)    
+    limit_decayrate = convert.fluxscale_to_invsec(limit)    
     limit_g = convert.decayrate_to_axion_g(limit_decayrate, m)    
     # output 
     limits_path = ("{}/JWST-NIRSPEC-limits.dat"
@@ -220,7 +223,7 @@ if __name__ == "__main__":
     # print("total time: {:0.1f} min".format(dt_total))
     # # convert to physics units
     # m = convert.wavelength_to_mass(lam_test)
-    # limit_decayrate = convert.fluxscale_to_invsec(limit, assume.rho_s, assume.r_s)    
+    # limit_decayrate = convert.fluxscale_to_invsec(limit)    
     # limit_g = convert.decayrate_to_axion_g(limit_decayrate, m)    
     # # output 
     # limits_path = ("{}/JWST-NIRSPEC-limits.dat"

sub-line-fit-full-multi.py
@@ -246,46 +246,49 @@ def find_raw_limit(setup_params, data, lam0):
 
 def find_pc_limit(Ntrials, Nbins, power_threshold, 
                  setup_params, data, fit_region):
-    [lam0, lam_list_msk, error_list_msk, 
-     knots, best_knots, spec_list] = fit_region
-    num_knots = setup_params["num_knots"]
-    guess = np.concatenate((best_knots, [0.0])) # fit starting
+    try:
+        [lam0, lam_list_msk, error_list_msk, 
+         knots, best_knots, spec_list] = fit_region
+        num_knots = setup_params["num_knots"]
+        guess = np.concatenate((best_knots, [0.0])) # fit starting
 
-    # generate mock data    
-    upper_limits = np.nan*np.ones(Ntrials)
-    for trial in range(Ntrials):
-        mock_data = []
-        trial_sky_list_msk = []
-        for i, spec_i in enumerate(spec_list):
-            mock_data.append({})
-            mock_data[-1]["max_res"] = data[spec_i]["max_res"]
-            mock_data[-1]["D"] = data[spec_i]["D"]
-            mock_data[-1]["lam"] = lam_list_msk[i]
-            mock_data[-1]["error"] = error_list_msk[i]
-            start = i*num_knots
-            end = (i + 1)*num_knots
-            Npts = error_list_msk[i].size
-            draw = np.random.normal(size=Npts)*error_list_msk[i]
-            continuum = interp.CubicSpline(knots[start:end], 
-                                           best_knots[start:end])
-            mock_data[-1]["sky"] = continuum(lam_list_msk[i]) + draw
-        out = find_raw_limit(setup_params, mock_data, lam0)
-        upper_limits[trial] = out[4][0]
+        # generate mock data    
+        upper_limits = np.nan*np.ones(Ntrials)
+        for trial in range(Ntrials):
+            mock_data = []
+            trial_sky_list_msk = []
+            for i, spec_i in enumerate(spec_list):
+                mock_data.append({})
+                mock_data[-1]["max_res"] = data[spec_i]["max_res"]
+                mock_data[-1]["D"] = data[spec_i]["D"]
+                mock_data[-1]["lam"] = lam_list_msk[i]
+                mock_data[-1]["error"] = error_list_msk[i]
+                start = i*num_knots
+                end = (i + 1)*num_knots
+                Npts = error_list_msk[i].size
+                draw = np.random.normal(size=Npts)*error_list_msk[i]
+                continuum = interp.CubicSpline(knots[start:end], 
+                                               best_knots[start:end])
+                mock_data[-1]["sky"] = continuum(lam_list_msk[i]) + draw
+            out = find_raw_limit(setup_params, mock_data, lam0)
+            upper_limits[trial] = out[4][0]
 
-    # integrate histogram
-    hist = np.histogram(upper_limits, bins=Nbins)
-    midpoints = 0.5*(hist[1][1:] +  hist[1][:-1])
-    pdf  = interp.interp1d(midpoints, hist[0], 
-                           bounds_error=False, fill_value=(0, 0))
-    rate_max = np.max(midpoints)
-    norm = integ.quad(pdf, 0, rate_max,
-                      epsrel=1e-4, epsabs=1e-4, limit=500)[0]
-    cdf_threshold = lambda gamma, threshold: (
-        integ.quad(pdf, 0, gamma, epsrel=1e-4, epsabs=1e-4, 
-                   limit=500)[0]/norm - threshold)
-    sol = opt.root_scalar(cdf_threshold, args=(power_threshold,),
-                          bracket=[0, rate_max])
-    return [lam0, sol.root]
+        # integrate histogram
+        hist = np.histogram(upper_limits, bins=Nbins)
+        midpoints = 0.5*(hist[1][1:] +  hist[1][:-1])
+        pdf  = interp.interp1d(midpoints, hist[0], 
+                               bounds_error=False, fill_value=(0, 0))
+        rate_max = np.max(midpoints)
+        norm = integ.quad(pdf, 0, rate_max,
+                          epsrel=1e-4, epsabs=1e-4, limit=500)[0]
+        cdf_threshold = lambda gamma, threshold: (
+            integ.quad(pdf, 0, gamma, epsrel=1e-4, epsabs=1e-4, 
+                       limit=500)[0]/norm - threshold)
+        sol = opt.root_scalar(cdf_threshold, args=(power_threshold,),
+                              bracket=[0, rate_max])
+        return [lam0, sol.root]
+    except:
+        return [np.nan, np.nan]
 
 
 if __name__ == "__main__":
@@ -299,12 +302,12 @@ if __name__ == "__main__":
     setup_params["window"] = width_factor*v_dm
     setup_params["padding"] = 1e-8
     setup_params["num_knots"] = 5
-    setup_params["chisq_step"] = 4 #2.71 
+    setup_params["chisq_step"] = 2.71 #4 
     setup_params["limit_guess"] = [1e-5, 1e-3]
     setup_params["max_clip_iters"] = 100
     setup_params["clipping_factor"] = 3
     # pc limit params 
-    pc_step_factor = 5 # subsampling factor for computing pc limit
+    pc_step_factor = 1 # subsampling factor for computing pc limit
     Ntrials = 10**2
     Nbins = 15
     power_threshold = 0.1587
@@ -362,11 +365,9 @@ if __name__ == "__main__":
 
     # physical conversion 
     m = convert.wavelength_to_mass(test_lams)
-    limit_decayrate = convert.fluxscale_to_invsec(
-        final_limits, assume.rho_s, assume.r_s)    
+    limit_decayrate = convert.fluxscale_to_invsec(final_limits)    
     limit_g = convert.decayrate_to_axion_g(limit_decayrate, m) 
-    bestfit_decayrate = convert.fluxscale_to_invsec(
-        bestfit, assume.rho_s, assume.r_s)    
+    bestfit_decayrate = convert.fluxscale_to_invsec(bestfit)    
     bestfit_g = convert.decayrate_to_axion_g(bestfit_decayrate, m) 
 
     # write output 