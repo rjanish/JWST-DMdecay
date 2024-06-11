98d93ae2633e72477ea6640e89563f86191946cf
rjanish <rjanish@berkeley.edu>
2024-06-11 17:09:28 -0500

    tried old injection run - need to revive old script

--- gnz11_split_injectiontest-noaddednoise.py
+++ gnz11_split_injectiontest-noaddednoise.py
@@ -57,7 +57,9 @@ if __name__ == "__main__":
     rawlimits = np.loadtxt(
         F"{run_name}/{prev_configs['run']['rawlimits_filename']}")
     print("Running injection test... ")
+    test_name = cur_configs["injection"]["name"]
     Ntrials = cur_configs["injection"]["Ntrials"]
+    input_factor = cur_configs["injection"]["input_factor"]
     valid = np.isfinite(rawlimits[:, 1])
     to_test = np.repeat(
         rawlimits[valid, :][::cur_configs["injection"]["skipping"], :],
@@ -70,7 +72,7 @@ if __name__ == "__main__":
         existing_limit = \
             dmd.conversions.invsec_to_fluxscale(decayrate_test) # code units
         test_results[i, 1] = existing_limit
-        G = existing_limit #*loguniform.rvs(0.33, 3) 
+        G = existing_limit*input_factor #*loguniform.rvs(0.33, 3) 
         test_results[i, 2] = G  # input signal
         for spec_index in range(len(trial_data)):
             # get signal sigma_lambda, combining instrumental and DM velocity 
@@ -91,33 +93,41 @@ if __name__ == "__main__":
                   "new_bf":new_rawlimits[5][0], 
                   "new_rawlimit":new_rawlimits[4][0]}
         output_path = \
-            F"{run_name}/injection/trial-old-{i}-{lam_test:0.6f}.pkl"
+            F"{run_name}/injection/trial-{test_name}-{i}-{lam_test:0.6f}.pkl"
         with open(output_path, "wb") as jar:
             pkl.dump(output, jar)
-    test_path = F"{run_name}/injection_results-old.dat"
+    test_path = F"{run_name}/{test_name}-injection_results-old.dat"
     np.savetxt(test_path, test_results)
 
     # print stats
-    discrepancy = (test_results[:, 4] - test_results[:, 2])/test_results[:, 1]
+    noise_estimate = np.min([np.median(spec["error"]) for spec in data])
+    discrepancy = (test_results[:, 4] - test_results[:, 2])/noise_estimate
     num_negative = np.sum(discrepancy < 0)
     frac_negative = num_negative/discrepancy.size
-    print(F"fraction of negative discrepancies: {100*frac_negative:0.4f}%")
+    expected_error = np.sqrt(1/discrepancy.size)
+    neg_frac_str = (F"negative fraction = {100*frac_negative:0.1f}%"
+                    F" +/- {100*expected_error:0.1f}%")
+    print(neg_frac_str)
 
     fig, ax = plt.subplots()
     ax.set_xlabel("relative discrepancy")
     ax.set_ylabel("count")
     ax.set_title("relative discrepancy between raw limit and injected flux")
-    Nhist = 20
-    positive_bins = np.linspace(0, discrepancy.max() + 1, Nhist)
-    positive_hist = ax.hist(discrepancy[discrepancy >= 0],
+    Nhist = 50
+    margin_factor = 1.1
+    positive_disc = discrepancy >= 0
+    max_right = discrepancy[positive_disc].max()*margin_factor
+    positive_bins = np.linspace(0, max_right, Nhist)
+    positive_hist = ax.hist(discrepancy[positive_disc],
                             bins=positive_bins, histtype='step', color='black')
     if num_negative > 0:
         bin_width = positive_hist[1][1] - positive_hist[1][0]
         negative_bins = -np.arange(0, -discrepancy.min(), bin_width)[::-1]
-        ax.hist(discrepancy[discrepancy < 0], bins=negative_bins, 
+        ax.hist(discrepancy[~positive_disc], bins=negative_bins, 
                 histtype='step', color='red')
     ax.axvline(0, color='gray', linestyle='--')
     ax.add_artist(
-        AnchoredText(F"negative fraction = {100*frac_negative:0.2f}%", loc='upper right', prop=dict(color="firebrick"), frameon=False))
-    fig.savefig(F"{run_name}/injection_discrepancy-old.png")
+        AnchoredText(neg_frac_str, loc='upper right', 
+                     prop=dict(color="firebrick"), frameon=False))
+    fig.savefig(F"{run_name}/{test_name}-injection_discrepancy.png")
     plt.show()
